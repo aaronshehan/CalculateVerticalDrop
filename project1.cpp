da6089328139eb8317d9c7761116a741fda529e5
soonersts28 <43185582+soonersts28@users.noreply.github.com>
2019-04-06 12:01:16 -0500

    Add files via upload

---
+++ project1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+/* Aaron Shehan  02/03/2018  CSCE 1030.001 
+This program will take user inputs for Initial Velocity and Final Velocity and use those inputs to output a value for Vertical Drop in meters and feet. */
+
+cout << "+----------------------------------------------+" << endl;
+cout << "|       Computer Science and Engineering       |" << endl;
+cout << "|        CSCE 1030 - Computer Science I        |" << endl;
+cout << "| Aaron Shehan ats0109 aaronshehan@my.unt.edu  |" << endl;
+cout << "+----------------------------------------------+" << endl;
+
+double const Gravity = 9.8; 		//Acceleration constant due to gravity
+double const Feet_conversion = 3.28; 	//For converting meters to feet
+double velocityInitial = 0; 		//For Initial Velocity
+double velocityFinal = 0; 		//For Final Velocity
+
+cout << "Enter the initial velocity at top of hill in m/s: ";
+cin >> velocityInitial; 
+cout << "Enter the final velocity at bottom of hill in m/s: ";
+cin >> velocityFinal;
+
+double verticalDrop = 0; //For relative vertical drop
+
+verticalDrop = (pow(velocityFinal, 2) - pow(velocityInitial, 2)) / (2 * Gravity); //Uses user input for initial and final velocity to calculate value of vertical drop
+
+cout << "The relative vertical drop of this roller coaster is ";
+cout << verticalDrop << " meters or ";
+cout << verticalDrop * Feet_conversion;
+cout << " feet." << endl;
+
+return 0;
+}
